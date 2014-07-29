eac9670a8bf532ff0269802a9ae4a777c1791cc0
Stanislav <stas19882004@gmail.com>
2014-07-29 16:14:40 -0400

    Init repo

---
+++ app/code/community/Stasevi4/Productlist/Helper/Data.php
@@ -0,0 +1,45 @@
+<?php
+
+class Stasevi4_Productlist_Helper_Data 
+    extends Mage_Core_Helper_Abstract
+    {
+        /**
+     * Array of available orders to be used for sort by
+     *
+     * @return array
+     */
+    public function getAvailableOrders()
+    {
+        return array(
+            // attribute name => label to be used
+            'price' => $this->__('Price'),
+			'sku' => $this->__('sku')
+        );
+    }
+
+    /**
+     * Return product collection to displayed by our list block
+     *
+     * @return Mage_Catalog_Model_Resource_Product_Collection
+     */
+    public function getProductCollection()
+    {
+        $rootCategoryId = Mage::app()->getStore()->getRootCategoryId();
+
+        $collection = Mage::getModel('catalog/category')
+            ->load(516)
+            ->getProductCollection()
+            ->addAttributeToSelect(Mage::getSingleton('catalog/config')->getProductAttributes())
+            ->addMinimalPrice()
+            ->addFinalPrice()
+            ->addTaxPercents()
+            ->addUrlRewrite($rootCategoryId);
+
+        Mage::getSingleton('catalog/product_status')->addVisibleFilterToCollection($collection);
+        Mage::getSingleton('catalog/product_visibility')->addVisibleInCatalogFilterToCollection($collection);
+
+        return $collection;
+    }
+    }
+
+?>
---
+++ app/code/community/Stasevi4/Productlist/controllers/AjaxController.php
@@ -0,0 +1,10 @@
+<?php
+
+class Stasevi4_Productlist_AjaxController extends Mage_Core_Controller_Front_Action {
+
+    public function indexAction()
+    {
+		$this->loadLayout();
+		$this->renderLayout();
+    }
+} 
