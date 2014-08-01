5cfc8025aaf7098ea378bc42971994f5d6809dc0
Stanislav <stas19882004@gmail.com>
2014-08-01 18:42:28 -0400

    Add category id in request

--- app/code/community/Stasevi4/Productlist/Helper/Data.php
+++ app/code/community/Stasevi4/Productlist/Helper/Data.php
@@ -8,15 +8,24 @@ class Stasevi4_Productlist_Helper_Data
      *
      * @return array
      */
+    protected $categoryid;	
+	
+	public function __construct(){
+	  	$this->setCategoryID(Mage::app()->getStore()->getRootCategoryId());
+	}
+	
     public function getAvailableOrders()
     {
         return array(
             // attribute name => label to be used
-            'price' => $this->__('Price'),
-			'sku' => $this->__('sku')
+            'Price' => $this->__('Price'),
+			'SKU' => $this->__('sku')
         );
     }
-
+    public function setCategoryID($value){
+	
+	    $this->categoryid = $value;
+	}
     /**
      * Return product collection to displayed by our list block
      *
@@ -24,22 +33,31 @@ class Stasevi4_Productlist_Helper_Data
      */
     public function getProductCollection()
     {
+	    if(Mage::app()->getRequest()->getParam('categoryid')){
+		   $this->setCategoryID(Mage::app()->getRequest()->getParam('categoryid')); 	
+		}        
+		
         $rootCategoryId = Mage::app()->getStore()->getRootCategoryId();
-
         $collection = Mage::getModel('catalog/category')
-            ->load(516)
+            ->load($this->categoryid)
             ->getProductCollection()
             ->addAttributeToSelect(Mage::getSingleton('catalog/config')->getProductAttributes())
             ->addMinimalPrice()
             ->addFinalPrice()
             ->addTaxPercents()
             ->addUrlRewrite($rootCategoryId);
-
+  /*      
+		$layer = Mage::getModel("catalog/layer");
+		$category = Mage::getModel("catalog/category")->load($this->categoryid);
+		$layer->setCurrentCategory($category);
+		$attributes = $layer->getFilterableAttributes();
+ */
+		
         Mage::getSingleton('catalog/product_status')->addVisibleFilterToCollection($collection);
         Mage::getSingleton('catalog/product_visibility')->addVisibleInCatalogFilterToCollection($collection);
 
         return $collection;
     }
-    }
+}
 
 ?>
--- app/code/community/Stasevi4/Productlist/controllers/AjaxController.php
+++ app/code/community/Stasevi4/Productlist/controllers/AjaxController.php
@@ -3,7 +3,8 @@
 class Stasevi4_Productlist_AjaxController extends Mage_Core_Controller_Front_Action {
 
     public function indexAction()
-    {
+    {   
+	     
 		$this->loadLayout();
 		$this->renderLayout();
     }
